0d6dfa53f313450b86499a390bcbe7029fb36ff6
Maxim Kazachuk <dem2bb1@gmail.com>
2020-12-23 19:51:28 +0200

    Готовое ДЗ для сдачи.

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,9 @@
-const Account = function (login, email) {
+const Account = function ({ login, email }) {
   this.login = login;
   this.email = email;
+};
+Account.prototype.getInfo = function () {
+  console.log(`Login: ${this.login}, Email: ${this.email}`);
 };
 
 console.log(Account.prototype.getInfo); // function
